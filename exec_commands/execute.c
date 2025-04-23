b10a9e9d988b514d4e40d9a0f81faaaafdf837d0
Gregorio Picchio <gpicchio@c1r4p9.42firenze.it>
2025-04-23 12:25:22 +0200

    redirectionsss

--- exec_commands/execute.c
+++ exec_commands/execute.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   execute.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vcastald <vcastald@student.42.fr>          +#+  +:+       +#+        */
+/*   By: gpicchio <gpicchio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/21 12:34:44 by gpicchio          #+#    #+#             */
-/*   Updated: 2025/04/15 15:08:01 by vcastald         ###   ########.fr       */
+/*   Updated: 2025/04/23 12:17:02 by gpicchio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -163,7 +163,7 @@ void	exec_piped_commands(t_gen *gen, t_tree *subroot)
 	pid_t		pid;
 
 	collect_piped_cmds(subroot, cmds, &num_cmds);
-
+	// printf("num_cmds: %d\n", num_cmds);
 	for (i = 0; i < num_cmds; i++)
 	{
 		if (i < num_cmds - 1 && pipe(pipe_fd) == -1)
@@ -183,13 +183,15 @@ void	exec_piped_commands(t_gen *gen, t_tree *subroot)
 		{
 			if (prev_fd != -1)
 			{
-				dup2(prev_fd, STDIN_FILENO);
+				dup2(prev_fd, cmds[i]->infile);
+				dup2(cmds[i]->infile, STDIN_FILENO);
+				dup2(cmds[i]->outfile, STDOUT_FILENO);
 				close(prev_fd);
 			}
 			if (i < num_cmds - 1)
 			{
 				close(pipe_fd[0]);
-				dup2(pipe_fd[1], STDOUT_FILENO);
+				dup2(pipe_fd[1], cmds[i]->outfile);
 				close(pipe_fd[1]);
 			}
 			exec_single_command(gen, cmds[i]);
@@ -202,6 +204,8 @@ void	exec_piped_commands(t_gen *gen, t_tree *subroot)
 			close(pipe_fd[1]);
 			prev_fd = pipe_fd[0];
 		}
+		printf("subroot->data->value: %s\n", cmds[i]->value);
+		printf("outfile: %d\n", cmds[i]->outfile);
 	}
 	for (i = 0; i < num_cmds; i++)
 	{
