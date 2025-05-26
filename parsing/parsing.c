e4310d68dcb3eed782c1b32b26d4e48b3a54dcee
gpicchio <gregoriopicchio@gmail.com>
2025-05-26 11:06:09 +0200

    ciao

--- parsing/parsing.c
+++ parsing/parsing.c
@@ -54,7 +54,8 @@ t_lexing	*filter_lexed_data(t_lexing *lexed_data)
 			if (!new_node)
 				return (NULL);
 			ft_lstadd_back(&head, new_node);
-			while (tmp && tmp->next && !stop_check(tmp) && !stop_check(tmp->next))
+			while (tmp && tmp->next
+					&& !stop_check(tmp->next) && !stop_check(tmp))
 				tmp = tmp->next;
 		}
 		if (tmp)
